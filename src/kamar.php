9aa2c48eb0ea94b8647bcae111dfe70bc1f41166
najwahidy <najwaahidyy@gmail.com>
2026-02-27 22:06:10 +0700

    jumlah.php

--- src/kamar.php
+++ src/kamar.php
@@ -1,1 +1,21 @@
-jumlah.php
+<?php
+$angka_rahasia = rand(1, 10);
+
+if (isset($_POST['tebak'])) {
+    $tebakan = $_POST['tebakan'];
+    if ($tebakan == $angka_rahasia) {
+        echo "Selang!";
+    } elseif ($tebakan < $angka_rahasia) {
+        echo "Terlalu rendah!";
+    } else {
+        echo "Terlalu tinggi!";
+    }
+}
+?>
+
+<form method="post">
+    <input type="number" name="tebakan">
+    <button type="submit" name="tebak">Tebak!</button>
+</form>
+
+
