3c1acb72d0622e1b89d804f77dc320f9eb5e3606
Andri Nur Ikhlas <247007111107@student.unsil.ac.id>
2026-02-27 16:05:07 +0700

    choru- folder test

---
+++ tests/test_login.py
@@ -0,0 +1,19 @@
+import unittest
+
+# Fungsi yang akan diuji
+def tambah(a, b):
+    return a + b
+
+class TestTambah(unittest.TestCase):
+    
+    def test_tambah_positif(self):
+        self.assertEqual(tambah(2, 3), 5)
+    
+    def test_tambah_negatif(self):
+        self.assertEqual(tambah(-1, -1), -2)
+    
+    def test_tambah_campuran(self):
+        self.assertEqual(tambah(5, -3), 2)
+
+if __name__ == '__main__':
+    unittest.main()
